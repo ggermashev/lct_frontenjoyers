c496937d1a14bcf08f4aac19ed724b4a11fe2628
ggermashev <ggermashev@gmail.com>
2022-11-03 21:01:34 +0300

    idk

--- project/lct4/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from lct4.views import *
-
-urlpatterns = [
-    path('registration/', Registration.as_view(), name='registration'),
-    path('login/', LoginUser.as_view(), name='login'),
-    path('profile/<slug:slug>', Profile.as_view(), name='profile'),
-    path('', Main.as_view(), name='main'),
-]
